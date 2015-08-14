7452624335213731788c2c614e0119d378890df4
Arif Islam <arif@dreamfactory.com>
2015-08-14 17:53:45 -0400

    fixing enterprise and df 2.0 integration after upgrading to L5.1

--- src/Providers/ManagedServiceProvider.php
+++ src/Providers/ManagedServiceProvider.php
@@ -15,9 +15,14 @@ class ManagedServiceProvider extends ServiceProvider
     //******************************************************************************
 
     /**
-     * @type string The key in config() to place the un/managed db config
+     * @type string The key in database config that holds all connections.
      */
-    const DATABASE_CONFIG_KEY = 'database.connections.dreamfactory';
+    const DATABASE_ALL_CONNECTIONS_KEY = 'database.connections';
+
+    /**
+     * @type string The key in database config that specifies default connection.
+     */
+    const DATABASE_DEFAULT_CONNECTION_KEY = 'database.default';
     /**
      * @type string The name of the service in the IoC
      */
@@ -35,8 +40,34 @@ class ManagedServiceProvider extends ServiceProvider
         //  Kick off the management interrogation
         Managed::initialize();
         $_dbConfig = Managed::getDatabaseConfig();
-        //  Stuff the db config into the config array
-        config([static::DATABASE_CONFIG_KEY => $_dbConfig]);
+
+        /*************************************************************************************************************
+         *
+         * To be efficient laravel DatabaseManager only creates connection once and it happens earlier during the
+         * db bootstrap process. So, by now it has already created a connection using the connection that's set in
+         * database.default config. Therefore, there is no point making changes in that connection (specified in
+         * database.default) config. Rather create a new connection and insert it into the database.connections
+         * array and set the database.default to this new connection.
+         */
+
+        //Creating new connection key using the md5 hash of the connection's database name.
+        $connectionKey = md5($_dbConfig['database']);
+
+        //Grabbing all available connections that's in database.connections config.
+        $allConnections = config(static::DATABASE_ALL_CONNECTIONS_KEY);
+
+        //Inserting the new connection config we got from enterprise console into list of all available connections.
+        $allConnections[$connectionKey] = $_dbConfig;
+
+        //Updating the database.connections config array.
+        config([static::DATABASE_ALL_CONNECTIONS_KEY => $allConnections]);
+
+        //Setting the default connection to use the newly inserted connection from enterprise console.
+        config([static::DATABASE_DEFAULT_CONNECTION_KEY => $connectionKey]);
+
+        /**
+         *************************************************************************************************************/
+
         logger('DB Config from Managed Service: ' . print_r($_dbConfig, true));
     }
 
