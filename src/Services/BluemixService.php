db86e6dd2ad93edc4d4c86a994cfd3ce84e2fe98
David Weiner <davidweiner@dreamfactory.com>
2016-01-12 16:49:46 -0500

    Commented out the cacheKey creation

--- src/Services/BluemixService.php
+++ src/Services/BluemixService.php
@@ -68,7 +68,7 @@ class BluemixService extends BaseService implements ProvidesManagedDatabase
      */
     public function getDatabaseConfig($service = self::BM_DB_SERVICE_KEY, $index = self::BM_DB_INDEX, $subkey = self::BM_DB_CREDS_KEY)
     {
-        $this->cacheKey = static::CACHE_KEY_PREFIX . app('request')->getHttpHost();
+        //$this->cacheKey = static::CACHE_KEY_PREFIX . app('request')->getHttpHost();
 
         //  Decode and examine
         try {
