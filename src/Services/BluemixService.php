505b8b16df0b26f8c74a4b5e5200d2e6cc221f2f
David Weiner <davidweiner@dreamfactory.com>
2016-01-19 15:15:36 -0500

    changed service name

--- src/Services/BluemixService.php
+++ src/Services/BluemixService.php
@@ -23,7 +23,7 @@ class BluemixService extends BaseService implements ProvidesManagedDatabase
     /**
      * @type string The name of the key containing the database
      */
-    const BM_DB_SERVICE_KEY = 'cleardb';
+    const BM_DB_SERVICE_KEY = 'user-provided';
     /**
      * @type int The index of the database to use
      */
