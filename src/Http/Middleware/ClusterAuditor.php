5cb1b607e86095c438628927edaa4b366cec38ab
Jerry Ablan <jerryablan@dreamfactory.com>
2016-02-04 14:15:17 -0500

    Exclude DFE Console metrics requests from being audited

--- src/Http/Middleware/ClusterAuditor.php
+++ src/Http/Middleware/ClusterAuditor.php
@@ -21,22 +21,25 @@ class ClusterAuditor
      */
     public function handle($request, Closure $next)
     {
-        try {
+        //  Don't log console requests
+        if (env('DF_IS_VALID_CONSOLE_REQUEST', false)) {
             try {
-                $_session = Session::getPublicInfo();
+                try {
+                    $_session = Session::getPublicInfo();
+                } catch (\Exception $_ex) {
+                    $_session = Session::all();
+                }
+
+                //  Register the auditing service
+                app()->register(AuditServiceProvider::class);
+
+                //  We use provider's service() method because Facades aren't loaded yet
+                AuditServiceProvider::service()->logRequest($request, $_session);
             } catch (\Exception $_ex) {
-                $_session = Session::all();
+                //  Completely ignored...
+                /** @noinspection PhpUndefinedMethodInspection */
+                Log::error('Exception during auditing: ' . $_ex->getMessage());
             }
-
-            //  Register the auditing service
-            app()->register(AuditServiceProvider::class);
-
-            //  We use provider's service() method because Facades aren't loaded yet
-            AuditServiceProvider::service()->logRequest($request, $_session);
-        } catch (\Exception $_ex) {
-            //  Completely ignored...
-            /** @noinspection PhpUndefinedMethodInspection */
-            Log::error('Exception during auditing: ' . $_ex->getMessage());
         }
 
         return $next($request);
